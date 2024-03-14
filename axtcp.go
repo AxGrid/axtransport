bda9f0ae58ca17144bdf82d6c2f02f3fe510b7e0
Dmitry <dmitry.vysochin@gmail.com>
2024-03-14 09:22:20 +0100

    tcp handler refactor

--- axtcp.go
+++ axtcp.go
@@ -3,8 +3,9 @@ package axtransport
 import (
 	"context"
 	"encoding/binary"
-	"github.com/pkg/errors"
+	"fmt"
 	"github.com/rs/zerolog"
+	"io"
 	"net"
 	"time"
 )
@@ -130,87 +131,69 @@ func (a *AxTcp) listen() {
 func (a *AxTcp) handleConn(conn net.Conn) {
 	log := a.logger.With().Str("remote", conn.RemoteAddr().String()).Logger()
 	defer conn.Close()
-	err := conn.SetReadDeadline(time.Now().Add(a.timeout))
-	if err != nil {
-		log.Error().Err(err).Msg("set read deadline failed")
-		return
-	}
-	if err != nil {
-		log.Error().Err(err).Msg("set read deadline failed")
-		return
-	}
 	axConn := NewAxTcpConnection(a.ctx, conn)
+	timeout := time.Duration(a.timeout) * time.Millisecond
 	defer axConn.Close()
-	dataChannel := make(chan []byte)
-	defer close(dataChannel)
-	go func() {
-		for {
-			data, ok := <-dataChannel
-			if !ok {
-				log.Info().Msg("channel closed")
-				return
-			}
-			data, err = a.binProcessor.Unmarshal(data)
-			if err != nil {
-				log.Error().Err(err).Msg("unmarshal failed")
-				return
-			}
-			go func(rData []byte) {
-				rData, err = a.handlerFunc(rData, axConn.ctx)
-				if err != nil {
-					log.Error().Err(err).Msg("handle request failed")
-					axConn.Close()
-					return
-				}
-				rData, err = a.binProcessor.Marshal(rData)
-				if err != nil {
-					log.Error().Err(err).Msg("marshal failed")
-					axConn.Close()
-					return
-				}
-				axConn.Write(addSize32(rData))
-			}(data)
+	for {
+		err := conn.SetReadDeadline(time.Now().Add(timeout))
+		if err != nil {
+			log.Error().Err(err).Msg("set read deadline failed")
+			return
 		}
-	}()
-	err = a.readerTL(conn, log, dataChannel)
-	if err != nil {
-		log.Error().Err(err).Msg("read error")
+		sizeBytes, err := readNBytes(conn, 4)
+		if err != nil {
+			log.Error().Err(err).Msg("failed to read header bytes")
+			break
+		}
+		bodyLength := getUInt32FromBytes(sizeBytes)
+		if bodyLength == 0 {
+			log.Error().Msg("failed to convert header to len")
+			break
+		}
+		err = conn.SetReadDeadline(time.Now().Add(timeout))
+		if err != nil {
+			log.Error().Err(err).Msg("failed to set body read deadline")
+			break
+		}
+		dataBytes, err := readNBytes(conn, int(bodyLength))
+		if err != nil {
+			log.Error().Err(err).Msg("failed to read body bytes")
+			break
+		}
+		data, err := a.binProcessor.Unmarshal(dataBytes)
+		if err != nil {
+			log.Error().Err(err).Msg("unmarshal failed")
+			break
+		}
+		go func(rData []byte) {
+			rData, err = a.handlerFunc(rData, axConn.ctx)
+			if err != nil {
+				log.Error().Err(err).Msg("handle request failed")
+				axConn.Close()
+				return
+			}
+			rData, err = a.binProcessor.Marshal(rData)
+			if err != nil {
+				log.Error().Err(err).Msg("marshal failed")
+				axConn.Close()
+				return
+			}
+			axConn.Write(rData)
+		}(data)
 	}
 }
 
-func (a *AxTcp) readerTL(conn net.Conn, log zerolog.Logger, dataChannel chan []byte) error {
-	defer conn.Close()
-	buf := make([]byte, 4096)
-	var data []byte
-	for {
-		i, err := conn.Read(buf)
-		if err != nil {
-			log.Error().Err(err).Msg("fail to read")
-			return err
-		}
-		if i == 0 {
-			log.Warn().Err(err).Msg("connection closed")
-			return errors.New("connection closed")
-		}
-		_ = conn.SetReadDeadline(time.Now().Add(time.Duration(a.timeout) * time.Millisecond))
-		data = append(data, buf[:i]...)
-		ix := 0
-		for { // Нужен если получили 2-ва пакета вместе
-			ix++
-			ld := len(data)
-			if ld >= 4 {
-				l4 := getUInt32FromBytes(data[:4])
-				if uint32(ld) >= l4+4 {
-					dataChannel <- data[4 : l4+4]
-					data = data[l4+4:]
-				} else {
-					break
-				}
-			} else {
-				break
-			}
-		}
+func readNBytes(conn net.Conn, n int) ([]byte, error) {
+	buff := make([]byte, n)
+	nRead, err := io.ReadFull(conn, buff)
+	if err != nil {
+		return nil, err
 	}
+	if nRead != n {
+		return buff, fmt.Errorf("failed to read packet in full")
+	}
+
+	return buff, nil
 }
 
 func getUInt32FromBytes(lens []byte) uint32 {
